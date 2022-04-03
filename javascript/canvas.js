34b32faf14b8b71520315db439116dd934e86d5e
aBoswell <A.B0ZZW3LL@gmail.com>
2022-04-03 10:36:57 -0400

    initial commit - directories added

---
+++ javascript/canvas.js
@@ -0,0 +1,118 @@
+// ***** ANY CLASSES GO UP TOP ***** 
+
+// I think we'll end up with two - one for each "falling object" allowing for mutliple generation.
+
+
+// *** ON WINDOW LOAD -> ANON FUNCTION = EVENT LISTNER ON START-BUTTON -> Start game function ***
+
+// In reviewing car game lab - looks like most if not all is within this start game function
+
+
+console.log('canvas file connected');
+
+window.onload = () => { // <-- DOM structure is not available until window load. 
+    document.getElementById('start-button').onclick = () => {
+      startGame();
+      console.log('canvas file connected');
+    };
+
+    // CONTEXT(game area) = 500(height) X 800(width) 
+
+    function startGame() {
+        const gameCanvas = document.getElementById('game-space');
+        const ctx = gameCanvas.getContext('2d'); // <-- method to obtain rendering context & drawing functions asigned to 'ctx' (so this is our context going forard).
+
+        let numFrame = 0;
+        //scoreCounter = 0;
+
+        const playerImage = new Image(); // <-- we load or player image using the image contstructor - which essentially creates a new element in the DOM.
+        playerImage.src = './images/player.png';
+
+        const playerObject = {
+            img: playerImage,
+            x: 0,
+            y: 0, 
+            width: 80,
+            height: 109,
+
+            moveLeft: function(){ // when called updates the objects x key value (negative = we reduce x = traverse left).
+                this.x -= 6;
+            },
+
+            moveRight: function(){
+                this.x += 6;
+            },
+
+            draw: function() { 
+                ctx.drawImage(this.img. this.x, this.y, this.width, this.height)
+            },
+    
+
+
+        };
+
+    
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    };
+
+    
+
+    //eventually load object images maybe? (donuts and bowling balls etc..) For now we start with shapes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
