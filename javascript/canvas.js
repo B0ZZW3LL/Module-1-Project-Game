e7f582f3949e1226921e6595d6d9333dca018f57
aBoswell <A.B0ZZW3LL@gmail.com>
2022-04-04 16:50:45 -0400

    game mechanics added, point gain and health loss, along with mousemove listner (control with mouse) & and end animation loop once health = 0

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,32 +1,70 @@
-// ***** ANY CLASSES GO UP TOP ***** 
-
-// I think we'll end up with two - one for each "falling object" allowing for mutliple generation.
-// In reviewing car game lab - looks like most if not all is within the below start game function.
-
 let scoreCounter = document.getElementById('score-counter');
+let gravity = 0.1;
 
-// *** ON WINDOW LOAD -> ANON FUNCTION = EVENT LISTNER ON START-BUTTON -> Start game function ***
+const donutImage = new Image();                                                                         // <-- we load our images using the image contstructor - which essentially creates a new element in the DOM.
+donutImage.src = './images/donut1.png';
+
+const ballImage = new Image();
+ballImage.src = './images/bowling_ball.png'
+
+class FallingObject {                                                                                   // <-- falling object generation/creation class
+    constructor(ctx, image, type){                                                                      // <-- when doing so, we'll provide our context (our 2D drawing surface), a randomly seleted image (donut or ball) & and a type 'donut' or 'bowling ball'.
+      this.ctx = ctx;
+      this.type = type
+      this.img = image;
+      this.width = 60;
+      this.height = 49;
+      this.x = Math.floor(Math.random() * (1000 - this.width + 1));                                     // <-- beginning x between 1 & 940 (canvas with minues object width).
+      this.y = -24;
+      this.vy = 1;                                                                                     // <-- negative should allow for the object to seem like it started a bit above canvas - rather than a object appearing instantly at top. 
+    }
+    
+    move(){
+      this.y += 1;
+    }
+    draw(){
+      this.ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+    leftBorder() {
+        return this.x;
+    }
+    rightBorder() {
+        return this.x + this.width;
+    }
+    topBorder() {
+        return this.y;
+    }
+    bottomBorder() {
+        return this.y + this.height;
+    }
+    isOffBottomOfCanvas(){                                                                              // <-- when invoked it will check for the object to see if y is greater than the canvas height.  
+       return this.y > 600;
+    }
+
+};
+
+// In reviewing race car game lab - looks like most if not all is within the below start game function.
 
 window.onload = () => {                                                                                 //<-- DOM structure is not available until window load.
-    console.log('canvas file connected'); 
     document.getElementById('start-button').onclick = () => {
       startGame();
-    };
-
-    // CONTEXT(game area) = 600(height) X 1000(width) 
+    };                                                                                                  // <--  CONTEXT(game area) = 600(height) X 1000(width) 
  
     function startGame() {
-        const gameCanvas = document.getElementById('game-space');
-        const ctx = gameCanvas.getContext('2d');                                                        // <-- method to obtain rendering context & drawing functions asigned to 'ctx' (so this is our context going forard).
+        const gameCanvas = document.getElementById('game-space');                                       // <-- we locate our canvas element in the DOM. 
+        const ctx = gameCanvas.getContext('2d');                                                        // <-- method to obtain rendering context & drawing functions asigned to 'ctx' (so this is our drawing surface going forard).
 
         let numFrame = 0;
-        scoreCounter.innerText = 0;                                                                     // *** DEFAULT THE SCORE LATER TO 0 IN HTML!!! ***
-
-        const playerImage = new Image();                                                                // <-- we load or player image using the image contstructor - which essentially creates a new element in the DOM.
+        let score = 0;
+        scoreCounter.innerText = score;                                                                 // <-- set score and corresponding DOM element "back to start" = 0 points/score.
+    
+        const playerImage = new Image();                                                                // <-- we load our player image and assign the source. 
         playerImage.src = './images/player.png';
 
         const playerObject = {
             img: playerImage,
+            health: 3,
+            vx: 5,                                                                                      // <-- rather than decrement points - let's go with "health system" - I.E gain points with each donut, if hit x amount of times then game stops... 
             x: 460,
             y: 491, 
             width: 80,
@@ -41,21 +79,88 @@ window.onload = () => {                                                         
             },
 
             draw: function() {
-                if(this.x > 940 ? this.x = 940 : this.x);                                               // <-- Try to keep the player in the game area
+                if(this.x > 940 ? this.x = 940 : this.x);                                               // <-- Try to keep the player within the game area
                 if(this.x <= 0 ? this.x = 0 : this.x);
                 ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
             },
 
-        };
+            left() {
+                return this.x;
+            },
+            right() {
+                return this.x + this.width;
+            },
+            top() {
+                return this.y;
+            },
+            bottom() {
+                return this.y + this.height;
+            },
+            crashWith(aFallingObject){ //access aligned bounding box
+                return !(this.bottom() < aFallingObject.topBorder() || this.top() > aFallingObject.bottomBorder() || this.right() < aFallingObject.leftBorder() || this.left() > aFallingObject.rightBorder());
+            }
+
+
+        }
+
+        const fallingObjectArray = [];
 
         function updateGameCanvas() {
+
+            if(numFrame % 70 === 0) {
+                let objectSelector = (Math.floor(Math.random() * 100) % 2 === 0) ?'A' :'B';             // <-- Randomly choose which "object image" to use, downside = what if they are different sizes.. 
+                    if(objectSelector === 'A') {
+                        fallingObjectArray.push(new FallingObject(ctx, donutImage, 'donut')); 
+                    } else {
+                       fallingObjectArray.push(new FallingObject(ctx, ballImage, 'bowling ball'));
+                    }
+            }
+        
             numFrame += 1;
 
             ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);                                   // <-- Clear the previous before drawing the new, else you'll get a "drag" effect. 
 
             playerObject.draw();
+            //playerObject.x += playerObject.vx;
 
-            requestAnimationFrame(updateGameCanvas);                                                    // <-- might want to change to setinterval after to run every 16 milliseconds - smooth out the animations
+            if(fallingObjectArray[0].isOffBottomOfCanvas()){                                            // <-- We assume and check if the first object remaining in the array is below the bottom border - then remove from array.  Seemed like good "house keeping" from race care lab. 
+                fallingObjectArray.shift();
+            }
+
+            for(let i = 0; i < fallingObjectArray.length; i++) {                                         //<-- Now for each object in our array we move and re-draw. 
+
+                fallingObjectArray[i].move();
+                fallingObjectArray[i].draw();
+                fallingObjectArray[i].vy += gravity;                                                    // <-- with each draw we apply "gravity" = acceleration on the y axies is increase with each "draw".
+                fallingObjectArray[i].y += fallingObjectArray[i].vy;
+            }
+            
+            for(let i = 0; i < fallingObjectArray.length; i++) { 
+                if(playerObject.crashWith(fallingObjectArray[i])) {
+
+                    if(fallingObjectArray[i].type === 'donut') {                                        // <-- if object collided with was a donut, add points... else it was a bowling ball so remove one from player health. 
+                        increaseScore();
+                    } else {
+                        reduceHealth();
+                        if(playerObject.health === 0) {
+                            return;                                                                     // <-- after checking if health is now 0, we'll return - stopping the updateGameCanvas "loop".
+                        }
+                    };
+
+                    fallingObjectArray.splice([i], 1);                                                  // <-- remove the object collided with from the array. 
+                }
+            }
+
+            stopId = window.requestAnimationFrame(updateGameCanvas);                                            // <-- might want to change to setinterval after to run every 16 milliseconds - smooth out the animations.
+        };
+
+        function increaseScore() {                                                                      // <-- increment "score" by 10 points and print latest "score" to DO, should only occur if object collided with was a donut.
+            score += 10;
+            scoreCounter.innerText = score;
+        };
+
+        function reduceHealth() {
+            playerObject.health --;
         };
 
         window.addEventListener('keydown', event => {
@@ -68,8 +173,12 @@ window.onload = () => {                                                         
                 break;
             }
         });
+        
+        window.addEventListener('mousemove', e => {
+            playerObject.x = e.clientX - gameCanvas.width/2;
+        });
 
-        updateGameCanvas();
+       updateGameCanvas();
 
     };
 
